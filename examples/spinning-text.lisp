f643857358e31e269730ae4569c265f74ae2e993
Anthony Fairchild <fairchild.anthony@gmail.com>
2025-08-11 09:20:42 -0400

    combined header with raylib.lisp file

--- examples/spinning-text.lisp
+++ examples/spinning-text.lisp
@@ -7,29 +7,42 @@
 
 (defparameter *default-font* nil)
 
-(let ((rotation 0.0))
-  (defun update ()
-    "Update the game state. This function can be expanded to include more complex logic."
-    ;; For now, we do nothing here.
-    (let* ((delta-time (get-frame-time))
-	   (font-spacing 1.0)
-	   (font-size 50.0)
- 	   (text (format nil "Hello, Gamebits! FPS=~A" (get-fps)))
-	   (text-size (measure-text-ex *default-font* text font-size font-spacing))
-	   (origin (make-vector2 :x (/ (vector2-x text-size) 2) :y (/ (vector2-y text-size) 2))))
-      (with-drawing ()
-	(clear-background +white+)
-	(draw-text-pro *default-font* text (make-vector2 :x 400 :y 300)
-		       origin rotation font-size font-spacing +blue+)
-	(incf rotation (* 90 delta-time 0.5))
-	))))
+(defparameter *rotation* 0.0)
+
+(defun update ()
+  "Update the game state. This function can be expanded to include more complex logic."
+  ;; For now, we do nothing here.
+  (let* ((delta-time (get-frame-time))
+	 (font-spacing 1.0)
+	 (font-size 50.0)
+ 	 (text (format nil "Hello, Gamebits! FPS=~A" (get-fps)))
+	 (text-size (measure-text-ex *default-font* text font-size font-spacing))
+	 (origin (make-vector2 :x (/ (vector2-x text-size) 2) :y (/ (vector2-y text-size) 2))))
+    (with-drawing ()
+      (clear-background +white+)
+      (draw-text-pro *default-font* text (make-vector2 :x 400 :y 300)
+		     origin *rotation* font-size font-spacing +blue+)
+      (incf *rotation* (* 90 delta-time 0.5))
+      )))
 
 (defun spinning-text ()
   (float-features:with-float-traps-masked (:overflow :invalid :divide-by-zero)
     (with-window (800 600 "Spinning Text Example")
-      (with-font (*default-font* "Roboto/static/Roboto-Regular.ttf" 50)
+      (with-font (*default-font* (namestring (asdf:system-relative-pathname :gamebits "Roboto/static/Roboto-Regular.ttf")))
+	;;(setf *default-font* (get-font-default))
 	(loop :until (window-should-close)
 	      :do (livesupport:continuable 
 		    (funcall 'update))
 		  (livesupport:update-repl-link))))))
 
+
+(defun test ()
+  (float-features:with-float-traps-masked (:overflow :invalid :divide-by-zero)
+    (load-font-ex 
+     (namestring
+      (asdf/system:system-relative-pathname
+       :gamebits
+       "Roboto/static/Roboto-Regular.ttf"))
+     50
+     (cffi:null-pointer)
+     0)))
