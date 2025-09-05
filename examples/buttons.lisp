a03ca38908bfadf8eb253de9a20e18245c3b572c
Anthony Fairchild <fairchild.anthony@gmail.com>
2025-09-05 16:15:33 -0400

    Cleaned up buttons example

--- examples/buttons.lisp
+++ examples/buttons.lisp
@@ -5,20 +5,20 @@
 
 (in-package #:gamebits/examples/buttons)
 
+(defparameter *toggled* nil)
 
-(let ((toggled nil))
- (defun update ()
-   (with-drawing ()
-     (clear-background (get-color (gui-get-style +DEFAULT+ +BACKGROUND-COLOR+)))
-       (when (gui-button (make-rectangle :x 100.0 :y 100.0 :width 200.0 :height 80.0) "Click Me!")
-	 (format t "Button Clicked!~%"))
-       (when (gui-checkbox (make-rectangle :x 100.0 :y 250.0 :width 20.0 :height 20.0) "Check Me!" toggled)
-	 (format t "Checkbox Toggled! ~A~%" toggled)))))
+(defun update ()
+  (with-drawing ()
+    (clear-background (get-color (gui-get-style +DEFAULT+ +BACKGROUND-COLOR+)))
+    (when (gui-button (make-rectangle :x 100.0 :y 100.0 :width 200.0 :height 80.0) "Click Me!")
+      (format t "Button Clicked!~%"))
+    (when (gui-checkbox (make-rectangle :x 100.0 :y 250.0 :width 20.0 :height 20.0) "Check Me!" *toggled*)
+      (format t "Checkbox Toggled! ~A~%" *toggled*))))
 
 (defun buttons()
   (with-window (800 600 "Buttons Example")
     (gui-set-font (get-font-default))
     (gui-set-style +DEFAULT+ +TEXT-SIZE+ 20)
     (loop :until (window-should-close)
-	  do (livesupport:continuable
+	  :do (livesupport:continuable
 	       (funcall 'update)))))
